f744c7f1f1df090b10dbb68a866ca8536bd1e407
divyahanda219 <divyahanda50@gmail.com>
2017-10-25 15:12:37 -0500

    calculates gc content

--- houseelfanalysis.R
+++ houseelfanalysis.R
@@ -4,3 +4,21 @@
 
 ##import data
 read.csv("data/houseelf_earlength_dna_data.csv")
+
+##Rename the file
+
+##gc content
+dataa <- read.csv("data/houseelf_earlength_dna_data_1.csv")
+print(dataa)
+
+##install.packages("stringr")
+library("stringr")
+
+gc_cont<- function(GC){
+  A <- str_count(dataa$dnaseq, "G")
+  B <- str_count(dataa$dnaseq, "c")
+output <- (A+B)/str_count(dataa)*100
+return(output)
+}
+gc_cont(dataa$dnaseq)
+
